9d7028c0d9a1cc6427ec1ba125509767f5611e1d
Sandrian W <sandrianw.work@gmail.com>
2022-03-08 10:36:00 +0700

    question 8

--- Forum Week 3/Question_8.java
+++ Forum Week 3/Question_8.java
@@ -1,5 +1,5 @@
 public class Question_8 {
-    public static void main(String[] args){
+    public static void main(String[] args){ //each line will print out the according astericks to draw the shape
         System.out.println( "   *********     ***       *       *       " ) ;
         System.out.println( "   *       *   *     *    ***     * *      " ) ;
         System.out.println( "   *       *  *       *  *****   *   *     " ) ;
