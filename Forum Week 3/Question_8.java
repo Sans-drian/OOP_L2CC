acce15db1254f614bb81083ee7da23c8618c053e
Sandrian W <sandrianw.work@gmail.com>
2022-03-08 10:30:09 +0700

    question 8

---
+++ Forum Week 3/Question_8.java
@@ -0,0 +1,14 @@
+public class Question_8 {
+    public static void main(String[] args){
+        System.out.println( "   *********     ***       *       *       " ) ;
+        System.out.println( "   *       *   *     *    ***     * *      " ) ;
+        System.out.println( "   *       *  *       *  *****   *   *     " ) ;
+        System.out.println( "   *       *  *       *    *    *     *    " ) ;
+        System.out.println( "   *       *  *       *    *   *       *   " ) ;
+        System.out.println( "   *       *  *       *    *    *     *    " ) ;
+        System.out.println( "   *       *  *       *    *     *   *     " ) ;
+        System.out.println( "   *       *   *     *     *      * *      " ) ;
+        System.out.println( "   *********     ***       *       *       " ) ;
+	} 
+
+}
